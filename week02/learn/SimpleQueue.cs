f5e497f4a3a5b3ea675d0a78e80039d2a37165a3
clarkedav <clarkdav282@gmail.com>
2026-01-19 18:14:40 +0100

    learning activity done

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -1,4 +1,81 @@
-﻿public class SimpleQueue {
+﻿public class SimpleQueue
+{
+    public static void Run()
+    {
+        // Test Cases
+
+        // Test 1
+        Console.WriteLine("Test 1");
+        var queue = new SimpleQueue();
+        queue.Enqueue(100);
+        var value = queue.Dequeue();
+        Console.WriteLine(value);
+
+        Console.WriteLine("------------");
+
+        // Test 2
+        Console.WriteLine("Test 2");
+        queue = new SimpleQueue();
+        queue.Enqueue(200);
+        queue.Enqueue(300);
+        queue.Enqueue(400);
+        value = queue.Dequeue();
+        Console.WriteLine(value);
+        value = queue.Dequeue();
+        Console.WriteLine(value);
+        value = queue.Dequeue();
+        Console.WriteLine(value);
+
+        Console.WriteLine("------------");
+
+        // Test 3
+        Console.WriteLine("Test 3");
+        queue = new SimpleQueue();
+        try
+        {
+            queue.Dequeue();
+            Console.WriteLine("Oops ... This shouldn't have worked.");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("I got the exception as expected.");
+        }
+    }
+
+    private readonly List<int> _queue = new();
+
+    /// <summary>
+    /// Enqueue the value provided into the queue
+    /// </summary>
+    /// <param name="value">Integer value to add to the queue</param>
+    private void Enqueue(int value)
+    {
+        // FIX #1: Add to the back of the queue
+        _queue.Add(value);
+    }
+
+    /// <summary>
+    /// Dequeue the next value and return it
+    /// </summary>
+    /// <exception cref="IndexOutOfRangeException">If queue is empty</exception>
+    /// <returns>First integer in the queue</returns>
+    private int Dequeue()
+    {
+        if (_queue.Count == 0)
+            throw new IndexOutOfRangeException();
+
+        // FIX #2: Remove from the front of the queue
+        var value = _queue[0];
+        _queue.RemoveAt(0);
+        return value;
+    }
+}
+
+
+
+
+
+/*public class SimpleQueue {
     public static void Run() {
         // Test Cases
 
@@ -70,4 +147,4 @@
         _queue.RemoveAt(1);
         return value;
     }
-}
+} */
